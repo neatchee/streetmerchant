211b29de6b622b85a1f5f1cc4ee68e8bcd833b88
neatchee <neatchee@gmail.com>
2020-12-29 01:37:28 -0800

    Specify links (including search + terms) to navigate to between product searches to reduce likelihood of bot detection/captcha When querying a store, if deterrentLinks has been defined, pick one at random and navigate there then wait 3 seconds

--- src/store/model/amazon.ts
+++ src/store/model/amazon.ts
@@ -8,8 +8,7 @@ export const Amazon: Store = {
 			'https://www.amazon.com/alm/storefront?almBrandId=VUZHIFdob2xlIEZvb2Rz&ref_=nav_cs_whole_foods_in_region',
 			'https://www.amazon.com/gp/goldbox?ref_=nav_cs_gb'
 		],
-		searchTerms: ['goober', 'dungeons+and+dragons'
-		],
+		searchTerms: ['goober', 'dungeons+and+dragons'],
 		searchUrl:
 			'https://www.amazon.com/s?k=%%s&i=todays-deals&ref=nb_sb_noss_2'
 	},
